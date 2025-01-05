9e3e65edfa7071b1e0bb09e1dc8eb3f30d8fcd60
luis enrique galup <legalup@pm.me>
2025-01-05 08:22:25 -0500

    huh. fixed a bunch of bugs. all better now

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ pub fn doit(nums: &mut Vec<(i32, i32)>) {
 
 struct GraphProblem {
     m: usize,
-    generator: usize,
+    i: usize,
     mid: usize,
     H: HashSet<usize>,
     iHm1: HashSet<usize>,
@@ -56,15 +56,24 @@ struct GraphProblem {
 
 impl GraphProblem {
     /// The values of `b` and `c` are always 0 in the provided inputs.
-    fn new(m: usize, generator: usize) -> GraphProblem {
+    fn new(m: usize, i: usize) -> GraphProblem {
         GraphProblem {
             m,
-            generator,
+            i,
             mid: (m - 1) / 2,
             H: HashSet::<usize>::new(),
             iHm1: HashSet::<usize>::new(),
-            graph : DirectedGraph::new(m, (m+1)),
+            graph: DirectedGraph::new(m, (m + 1)),
         }
+    }
+
+    fn domod(&self, x : usize) -> i32 
+    {
+        let mut ix = x as i32;
+        if ix > (self.mid as i32) {
+            ix -= (self.m as i32)
+        };
+        ix
     }
 
     pub fn findnextvert(&mut self) -> Option<usize> {
@@ -72,7 +81,7 @@ impl GraphProblem {
             return Some(1usize);
         }
 
-        if self.H.len() == self.mid {
+        if self.H.len() == self.m {
             return None;
         }
 
@@ -85,10 +94,10 @@ impl GraphProblem {
         None
     }
 
-    pub fn add_forward_cycle_directed(&mut self, x: usize) -> usize {
-        let y = (x * self.generator) % self.m;
-        let z = (y * self.generator) % self.m;
-        let w = (z * self.generator) % self.m;
+    pub fn add_forward_cycle_directed(&mut self, x: usize, generator: usize) -> usize {
+        let y = (x * generator) % self.m;
+        let z = (y * generator) % self.m;
+        let w = (z * generator) % self.m;
 
         self.graph.add_edge(x, y);
         self.graph.add_edge(y, x);
@@ -102,44 +111,27 @@ impl GraphProblem {
         self.graph.add_edge(w - 1, x - 1);
         self.graph.add_edge(x - 1, w - 1);
 
-        //augment zm
-        if x <= self.mid {
-            self.H.insert(x);
-        };
-        if y <= self.mid {
-            self.H.insert(y);
-        };
-        if z <= self.mid {
-            self.H.insert(z);
-        };
-        if w <= self.mid {
-            self.H.insert(w);
-        };
+        self.H.insert(x);
+        self.H.insert(y);
+        self.H.insert(z);
+        self.H.insert(w);
 
-        if x <= self.mid {
-            self.iHm1.insert(x);
-        };
-        if y <= self.mid {
-            self.iHm1.insert(y);
-        };
-        if z <= self.mid {
-            self.iHm1.insert(z);
-        };
-        if w <= self.mid {
-            self.iHm1.insert(w);
-        };
+        self.iHm1.insert(x - 1);
+        self.iHm1.insert(y - 1);
+        self.iHm1.insert(z - 1);
+        self.iHm1.insert(w - 1);
 
         self.H.len()
     }
-
 
     pub fn print_conn_comps(&self) {
         let cg = ConnectivityDirectedGraph::new(&self.graph);
         //println!("the number of connected components is {} ", cg.num_cc);
         //println!("the coloring is {:?}", cg.cc);
-        let mut freq = HashMap::<usize, Vec<usize>>::new();
+        let mut freq = HashMap::<usize, Vec<i32>>::new();
         for (idx, ii) in cg.cc.iter().enumerate() {
-            freq.entry(*ii).and_modify(|v| v.push(idx)).or_default();
+            freq.entry(*ii).and_modify(|v| v.push(self.domod(idx))).
+            or_insert(Vec::from([self.domod(idx)]));
         }
 
         for key in freq.keys() {
@@ -150,31 +142,27 @@ impl GraphProblem {
     }
 
     pub fn print_adj_list(&self) {
-        let mut domod = |x: usize| -> i32 {
-            let mut ix = x as i32;
-            if ix > (self.mid as i32) {
-                ix -= (self.m as i32)
-            };
-            ix
-        };
 
         (0..self.m).for_each(|x| {
-            print!("for vertex {}:", domod(x));
+            print!("for vertex {}:", self.domod(x));
             for y in self.graph.adj_list(x) {
-                print!("{},", domod(y.1))
+                print!("{},", self.domod(y.1))
             }
             //{ let z = y.1; let mut iz = z as i32; if iz>14 {iz -= (moddy as i32); print!("{},",iz)};}
             println!("---")
         });
     }
 
-    pub fn solve(&mut self)
-    {
+    pub fn solve(&mut self) {
+        let mut counter = 0usize;
+        let j = (self.i*self.i*self.i) % self.m;
         while let Some(v) = self.findnextvert() {
-            self.add_forward_cycle_directed(v);
-            println!("here is v {} and H {:?}", v, self.H);
+            let generator = if counter % 2 == 0 {self.i} else {j};
+            self.add_forward_cycle_directed(v, generator);
+            println!("here is v {} and H {:?} and iHm1 {:?}", v, self.H, self.iHm1);
             self.print_conn_comps();
             println!("---------------");
+            counter += 1;
         }
     }
 }
@@ -187,10 +175,9 @@ fn main() -> io::Result<()> {
 
     let m = 29usize;
     let i = 12usize;
-    let mut testy  = GraphProblem::new(m,i);
+    let mut testy = GraphProblem::new(m, i);
 
     testy.solve();
-    
 
     Ok(())
 }
